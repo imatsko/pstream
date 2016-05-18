9ee213e594a9ebb530a3cfe9d170fa09131a9936
Igor Matsko <rigelrisen@gmail.com>
2016-05-18 20:59:11 +0300

    add pprof

--- run2/run.go
+++ run2/run.go
@@ -7,9 +7,13 @@ import (
 	"github.com/imatsko/pstream"
 	"github.com/op/go-logging"
 	"io/ioutil"
-	"math/rand"
+	_ "math/rand"
 	"strings"
 	"time"
+	"log"
+	_ "net/http/pprof"
+
+	"net/http"
 )
 
 var main_log = logging.MustGetLogger("Main")
@@ -32,10 +36,13 @@ var Config = struct {
 	SourceChunks  uint64         `json:"count"`
 	SendPeriod    time.Duration  `json:"send_period"`
 	BootstrapList StringComaList `json:"bootstrap"`
+	PprofListen   string         `json:"pprof"`
+
 }{
 	ConfFile:      "config.json",
 	Listen:        "127.0.0.1:9000",
 	SourceChunks:  10000,
+	PprofListen: "",
 	SendPeriod:    time.Millisecond * 100,
 	BootstrapList: StringComaList(make([]string, 0)),
 }
@@ -45,6 +52,8 @@ var configLogger, _ = logging.GetLogger("config")
 func init() {
 	flag.StringVar(&Config.ConfFile, "conf", Config.ConfFile, "Read config from conf file if it exists")
 	flag.StringVar(&Config.Listen, "listen", Config.Listen, "Listen on addr:port ")
+
+	flag.StringVar(&Config.PprofListen, "pprof", Config.PprofListen, "Listen pprof on addr:port ")
 
 	flag.BoolVar(&Config.Source, "source", Config.Source, "Generate stream")
 	flag.Uint64Var(&Config.SourceChunks, "chunks", Config.SourceChunks, "Source shunk count")
@@ -74,30 +83,38 @@ func start_source() {
 	go p1.Serve()
 
 	send := func() {
+		//var chunk_size int
+		//chunk_size = int(1024*1024*time.Second/pstream.STREAM_CHUNK_PERIOD)
 		for i := uint64(1); i <= Config.SourceChunks; i += 1 {
+			//buf_data := make([]byte,chunk_size)
+			//for i := 0; i < 100; i++ {
+			//	pos := rand.Intn(chunk_size)
+			//	buf_data[pos] = byte(i)
+			//}
 
-			time.Sleep(pstream.STREAM_CHUNK_PERIOD)
+			//c := pstream.Chunk{uint64(i), &buf_data}
 			c := pstream.Chunk{uint64(i), i}
-			main_log.Debugf("Sending %#v", c)
+			main_log.Debugf("Sending %v", c.Id)
 			in <- &c
-			main_log.Debugf("Sending %#v finished", c)
+			main_log.Debugf("Sending %v finished", c.Id)
+			time.Sleep(pstream.STREAM_CHUNK_PERIOD)
 			continue
 
-			if rand.Intn(4) != 0 {
-				time.Sleep(pstream.STREAM_CHUNK_PERIOD)
-				c := pstream.Chunk{uint64(i), i}
-				main_log.Debugf("Sending %#v", c)
-				in <- &c
-				main_log.Debugf("Sending %#v finished", c)
-			} else if rand.Intn(3) != 0 {
-				time.Sleep(2 * pstream.STREAM_CHUNK_PERIOD)
-				c := pstream.Chunk{uint64(i), i}
-				main_log.Debugf("Sending slow %#v", c)
-				in <- &c
-				main_log.Debugf("Sending slow %#v finished", c)
-			} else {
-				main_log.Debugf("drop chunk %v", i)
-			}
+			//if rand.Intn(4) == 0 {
+			//	time.Sleep(pstream.STREAM_CHUNK_PERIOD)
+			//	c := pstream.Chunk{uint64(i), i}
+			//	main_log.Debugf("Sending %v", c.Id)
+			//	in <- &c
+			//	main_log.Debugf("Sending %v finished", c.Id)
+			//} else if rand.Intn(3) == 0 {
+			//	time.Sleep(2 * pstream.STREAM_CHUNK_PERIOD)
+			//	c := pstream.Chunk{uint64(i), i}
+			//	main_log.Debugf("Sending slow %v", c.Id)
+			//	in <- &c
+			//	main_log.Debugf("Sending slow %v finished", c.Id)
+			//} else {
+			//	main_log.Debugf("drop chunk %v", i)
+			//}
 		}
 	}
 
@@ -105,7 +122,7 @@ func start_source() {
 		for {
 			select {
 			case c := <-out:
-				main_log.Debugf("Received %#v", c)
+				main_log.Debugf("Received %v", c.Id)
 			case <-time.After(time.Second * 30):
 				main_log.Debugf("No new packages too long")
 				p1.Exit()
@@ -134,7 +151,7 @@ func start_peer() {
 		for {
 			select {
 			case c := <-out:
-				main_log.Debugf("Received %#v", c)
+				main_log.Debugf("Received %v", c.Id)
 			case <-time.After(time.Second * 30):
 				main_log.Debugf("No new packages too long")
 				p1.Exit()
@@ -147,6 +164,12 @@ func start_peer() {
 
 func main() {
 	main_log.Infof("Start with config %+v", Config)
+
+	if Config.PprofListen != "" {
+		go func() {
+			log.Println(http.ListenAndServe(Config.PprofListen, nil))
+		}()
+	}
 
 	if Config.Source {
 		start_source()
